35a694eca75e392f39b64749ef0592f3d571286b
Shantito10 <chantelmantentela@gmail.com>
2023-10-03 10:27:41 -0700

    6. combination brains

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for i in range(0, 9):
     for y in range(i + 1, 10):
         if i is not 8:
-            print("{}{},".format(i, y), end='')
+            print("{}{},".format(i, y), end=' ')
         else:
             print("{}{}".format(i, y))
