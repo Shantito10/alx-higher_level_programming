98ea1b46afcb36bd5e692c349274ed86e4ded77e
Shantito10 <chantelmantentela@gmail.com>
2023-10-03 10:19:52 -0700

    6. combination

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range(1, 9):
-    for y in range(i + 1, 9):
+for i in range(0, 9):
+    for y in range(i + 1, 10):
         print("{:02d}, ".format(i * 9 + y), end="")
     if i != 8:
         print("{:02d}, ".format(i * 9 + 9), end="")
