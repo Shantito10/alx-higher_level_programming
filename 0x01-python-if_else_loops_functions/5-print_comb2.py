573f0fe6af13e893647ef978b2b660ceb3514165
Shantito10 <chantelmantentela@gmail.com>
2023-10-03 09:49:49 -0700

    00...99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range(0, 100):
-    if i is not  99:
-        print("{:0>2d}, ".format(i), end='')
+for i in range(100):
+    if i < 99:
+        print(f"{i:02d}, ", end='')
     else:
-        print("{}".format(i))
+        print(f"{i:02d}")
