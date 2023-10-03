9b934ff7a1aed2863b0e8697018c54e734d7b82a
Shantito10 <chantelmantentela@gmail.com>
2023-10-03 14:25:27 -0700

    bytecode -> python #2

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def magic_culculation(a, b, c):
+    if a < b:
+        return c
+    elif a > c:
+        return b
+    else:
+        return a * n - c
---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def uppercase(input_str):
+    for c in input_str:
+        print(chr(ord(c) - 32, end="")
+    print()
