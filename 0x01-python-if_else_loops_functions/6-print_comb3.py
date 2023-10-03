f1780173d1c35754a1b345aae06c89c6dfd5f7d6
Shantito10 <chantelmantentela@gmail.com>
2023-10-03 15:59:35 -0700

    bain of combination

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for i in range(9):
     for j in range(i + 1, 10):
-        if i is not 8:
+        if i != 8:
             print("{}{},".format(i, j), end=' ')
         else:
             print("{}{}".format(i, j))
