97ba35be228d37835956c4d42291a0c020e95245
Shantito10 <chantelmantentela@gmail.com>
2023-10-03 17:30:05 -0700

    chantel

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for i in range(1, 101):
-        if 1 % 3 and i % 5:
+        if i % 3 and i % 5:
             print("{:d}".format(i), end=" ")
             continue
         if i % 3 == 0:
