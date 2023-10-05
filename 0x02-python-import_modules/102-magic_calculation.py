029deeaa800e25233331f4dcca1438852db0cfb7
Shantito10 <chantelmantentela@gmail.com>
2023-10-05 06:49:01 -0700

    bytecode-> python3

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
-def magic_calculation(a, b):
+def magic_culculation(a, b):
     if a < b:
+        add = __import__('magic_culculation_102').add
+        sub = __import__('magic_culculation_102').sub
         c = add(a, b)
+
         for i in range(4, 6):
             c = add(c, i)
         return c
-    return sub(a, b) if True else None
+    else:
+        return sub(a, b)
