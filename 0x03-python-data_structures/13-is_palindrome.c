ca55fdf3cff25e4d13ddc9e9453e819dd8bed306
Shantito10 <chantelmantentela@gmail.com>
2023-10-09 12:15:31 -0700

    13.linked list

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+int is_really_palin(listint_t **head, listint_t *next);
+/**
+ * is_palindrome - checks if a singly linked list is a palindrome
+ * @head: head pointer to a linked list
+ * Return: 0 if not a palindrome, 1 if it is a palindrome
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *next;
+
+	if (head == NULL)
+		return (0);
+	if (*head == NULL || (*head)->next == NULL)
+		return (1);
+	next = *head;
+	return (is_really_palin(head, next->next));
+}
+
+/**
+ * is_really_palin - helps check if a linked list in a palin
+ * @head: head pointer to a linked list
+ * @next: pointer to a linked list
+ * Return: correct if the values are equal
+ */
+int is_really_palin(listint_t **head, listint_t *next)
+{
+	if (next->next != NULL)
+		is_really_palin(head, next->next);
+	if ((*head)->n == next->n)
+	{
+		*head = (*head)->next;
+		return (1);
+	}
+	else
+		return (0);
+}
