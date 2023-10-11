eef03a0ed74cfe190153bdbff888b6399a62e60e
Shantito10 <chantelmantentela@gmail.com>
2023-10-11 13:55:23 -0700

    12.roman

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,8 +5,7 @@ def roman_to_int(roman_string):
 
     roman_dict = {
         'I': 1, 'V': 5, 'X': 10, 'L': 50,
-        'C': 100, 'D': 500, 'M': 1000
-    }
+        'C': 100, 'D': 500, 'M': 1000, 'CM': -100, 'XC': -10}
 
     result = 0
     prev_value = 0
