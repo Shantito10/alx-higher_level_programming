7b374c8c4997d5f7286bc33fd1e38313cc530d3c
Shantito10 <chantelmantentela@gmail.com>
2023-11-01 14:31:46 -0700

    more classes

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -3,20 +3,33 @@ import sys
 
 def is_safe(board, row, col, N):
     for i in range(col):
-        if board[row][i] == row or board[i] - i == row - col or board[i] + i == row + col:
+        if board[row][i] == 1:
             return False
-        return True
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+    for i, j in zip(range(row, N, 1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+    return True
 
 def solve_nqueens_util(board, col, N, result):
-    if col == N:
-        solution = [[i, board[i]] for i in range(N)]
+    if col >= N:
+        solution = []
+        for i in range(N):
+            for j in range(N):
+                if board[i][j] == 1:
+                    solution.append([i, j])
         result.append(solution)
-        return
+        return True
 
+    res = False
     for i in range(N):
         if is_safe(board, i, col, N):
-            board[col] = i
-            solve_nqueens_util(board, col + 1, N, result)
+            board[i][col] = 1
+            res = solve_nqueens_util(board, col + 1, N, result) or res
+            board[i][col] = 0
+    return res
 
 def solve_nqueens(N):
     if not isinstance(N, int):
@@ -26,7 +39,7 @@ def solve_nqueens(N):
         print("N must be at least 4")
         sys.exit(1)
 
-    board = [-1] * N
+    board = [[0 for _ in range(N)] for _ in range(N)]
     result = []
     solve_nqueens_util(board, 0, N, result)
     for sol in result:
